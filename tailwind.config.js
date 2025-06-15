7dde848ff7bd57d1a4ea947668a9ca06fb2d5e4e
Vlad Lesun <vladlesunmma@gmail.com>
2025-06-15 16:08:15 +0300

    feat: add html, styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 const plugin = require('tailwindcss/plugin');
+console.log('plugin: ', plugin);
 
 module.exports = {
 	content: ['./src/**/*.{html,js}'],
 	darkMode: 'class',
 	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				sm: '100%',
+				md: '100%',
+				lg: '100%',
+				xl: '100%',
+			},
+		},
 		extend: {
 			colors: {
 				primary: '#1E3A8A',
@@ -13,7 +24,7 @@ module.exports = {
 				neutral: '#F3F4F6',
 			},
 			fontFamily: {
-				sans: ['"Open Sans"', 'sans-serif', 'system-ui'],
+				sans: ['"Open Sans"', 'sans-serif'],
 			},
 			spacing: {
 				128: '32rem',
